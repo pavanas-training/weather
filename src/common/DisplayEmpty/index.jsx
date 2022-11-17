acebca7210d83d876542ff23bb526b49aad185e2
Pavana S Salvankar <pavana_salvankar@discovery.com>
2022-11-17 16:41:24 +0530

    rename

--- src/common/DisplayEmpty/index.jsx
+++ src/common/DisplayEmpty/index.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import styled from "styled-components";
-import icon from "../../assets/icons/icon_nothing.svg";
+import React from 'react';
+import styled from 'styled-components';
+import icon from '../../assets/icons/icon_nothing.svg';
 const DisplayEmpty = ({ msg }) => {
   return (
     <DisplayEmptyContainer>
-      <StyledNothingIcon src={icon}></StyledNothingIcon>
+      <StyledIcon src={icon}></StyledIcon>
       <StyledMessage>{msg}</StyledMessage>
     </DisplayEmptyContainer>
   );
 };
 const DisplayEmptyContainer = styled.div``;
-const StyledNothingIcon = styled.img`
+const StyledIcon = styled.img`
   width: 12%;
   height: 15%;
   padding-top: 15%;
@@ -19,7 +19,7 @@ const StyledMessage = styled.h1`
   height: 21px;
   width: 100%;
   color: #ffffff;
-  font-family: "Roboto", sans-serif;
+  font-family: 'Roboto', sans-serif;
   font-size: 1.125rem;
   font-weight: 500;
   letter-spacing: 0;
